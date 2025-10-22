b7ab9ddfae53dc47f150020a35fa5f4231b26f54
Naveen Basavaraj <61464089+NaveenBasavaraj@users.noreply.github.com>
2025-10-22 18:48:14 +0530

    added employee and poll models

--- employee/models.py
+++ employee/models.py
@@ -1,3 +1,14 @@
 from django.db import models
-
+from django.contrib.auth.models import User 
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    designation = models.CharField(max_length=20, null=False, blank=False)
+    salary = models.IntegerField()
+
+    class Meta:
+        ordering = ('-salary',)
+
+    def __str__(self):
+        return self.user.firstname
--- poll/models.py
+++ poll/models.py
@@ -1,3 +1,26 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+class Question(models.Model):
+    title = models.TextField(null=False)
+    is_active = models.BooleanField(default=False)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    create_user = models.ForeignKey(User, on_delete=models.CASCADE)
+    create_ts = models.DateTimeField(auto_now_add=True)
+    mod_ts = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Choice(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.TextField(null=False)
+    create_ts = models.DateTimeField(auto_now_add=True)
+    mod_ts = models.DateField(auto_now=True)
+
+    def __str__(self):
+        return self.text
+    
